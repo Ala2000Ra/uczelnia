29152c6008033d2c09603ee5880c8af70efa54e3
Alicja Radke <alicja.radke0306@gmail.com>
2023-05-14 18:56:36 +0200

    zadanie 10

--- solo-work/solo_1.py
+++ solo-work/solo_1.py
@@ -54,3 +54,40 @@ for student in studenci:
     if student.split()[1][0] == "N":
         liczba_n +=1
 print("Liczba studentow na N wynosi: " + str(liczba_n))
+
+
+#zad 1.10
+wykres_1 = [[2, 4], [4, 4], [6, 4]]
+wykres_2 = [[2, 3], [4, 4], [6, 5]]
+wykres_3 = [[2, 3], [4, 3], [5, 4]]
+
+def czy_liniowa(wykres):
+    a = None
+    for i in range(len(wykres) - 1):
+        x1, y1 = wykres[i]
+        x2, y2 = wykres[i+1]
+        spra = (y2 - y1)/(x2-x1)
+        if a is None:
+            a = spra
+        elif spra != a:
+            return False        
+    return True
+    
+wykres_1_funkcja_liniowa = czy_liniowa(wykres_1)
+wykres_2_funkcja_liniowa = czy_liniowa(wykres_2)
+wykres_3_funkcja_liniowa = czy_liniowa(wykres_3)
+
+if wykres_1_funkcja_liniowa:
+    print("Dla punktow w wykres_1 mozna wyznaczyc funkcje liniowa.")
+else:
+    print("Dla punktow w wykres_1 nie mozna wyznaczyc funkcji liniowej.")
+
+if wykres_2_funkcja_liniowa:
+    print("Dla punktow w wykres_2 mozna wyznaczyc funkcje liniowa.")
+else:
+    print("Dla punktow w wykres_2 nie mozna wyznaczyc funkcji liniowej.")
+
+if wykres_3_funkcja_liniowa:
+    print("Dla punktow w wykres_3 mozna wyznaczyc funkcje liniowa.")
+else:
+    print("Dla punktow w wykres_3 nie mozna wyznaczyc funkcji liniowej.")
